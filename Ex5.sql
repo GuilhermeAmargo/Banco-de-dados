975d95deee363be05f0ddd3ef2b239bd44c705e7
GuilhermeAmargo <guilhermecamargosilva211@gmail.com>
2024-03-20 21:59:55 -0300

    Não finalizado

---
+++ Ex5.sql
@@ -0,0 +1,10 @@
+select * from livros
+select * from autores
+select * from livros_autores
+
+drop table autores cascade;
+insert into autores (id_autor,nome_autor) values
+('1123134','Harper Lee'),
+('4245535','Dan Brown'),
+('6263636','George Orwell');
+
