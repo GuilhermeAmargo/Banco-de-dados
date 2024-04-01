1d6226b73c6fba7a9daeb03e1f2135097339fe99
GuilhermeAmargo <159201870+GuilhermeAmargo@users.noreply.github.com>
2024-04-01 16:57:12 -0300

    Complete

--- Ex4.sql
+++ Ex4.sql
@@ -62,7 +62,7 @@ SELECT nome_medico FROM (medicos natural inner join pacientes_medicos) inner joi
 where nome_paciente = 'Daniel Rocha'
 
 -- Contar o número médio de pacientes por médico
-SELECT AVG(num_pacientes) AS media_pacientes_por_medico
+SELECT AVG(num_pacientes) AS media_pacientes_por_medico;
 FROM (
     SELECT id_medico, COUNT(id_paciente) AS num_pacientes
     FROM pacientes_medicos
